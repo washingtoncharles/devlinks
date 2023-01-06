62d3f5f767035fb2bb2a4260bc58e4ddff48dd4d
Washington Charles <toncharles@live.com>
2023-01-05 22:59:59 -0300

    update image on mode switch

--- scripts.js
+++ scripts.js
@@ -8,4 +8,12 @@ function toggleMode() {
   }*/
 
   html.classList.toggle('light-mode')
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains("light-mode")) {
+    img.setAttribute("src", "./assets/Avatar-light-mode.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
 }
